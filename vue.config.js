f9764cf69becdf8975eb3b950581e6e500650e3b
xuguoyuan <xuguoyuan@gsafety.com>
2021-03-05 16:18:19 +0800

    feat: gzip压缩

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,4 @@
+const CompressionPlugin = require("compression-webpack-plugin");
 const chunkVendorSplit = require('./scripts/chunk-vendor-split');
 
 module.exports = {
@@ -16,7 +17,17 @@ module.exports = {
   configureWebpack: config => {
     config.target = 'web';
     if (process.env.NODE_ENV === 'production') {
-      chunkVendorSplit.split(config);
+      // 不分开的时候更有利于整体加载
+      // chunkVendorSplit.split(config);
+      return {
+        plugins: [
+          new CompressionPlugin({
+            test: /\.js$|\.html$|\.css$|\.jpg$|\.jpeg$|\.png/, // 需要压缩的文件类型
+            threshold: 10240, // 归档需要进行压缩的文件大小最小值，我这个是10K以上的进行压缩
+            deleteOriginalAssets: true // 是否删除原文件
+          })
+        ]
+      }
     }
   }
 };
